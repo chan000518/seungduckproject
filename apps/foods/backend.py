311771faa5048da31b072d0441ab9070740cce48
chan000518 <128445671+chan000518@users.noreply.github.com>
2024-02-26 22:44:43 +0900

    [fix] 음식 추천 수정

--- apps/foods/backend.py
+++ apps/foods/backend.py
@@ -4,26 +4,30 @@ import openai
 from datetime import date
 # from apps.weather.models import Weather 
 
-System_Role="""당신은 한국에서 유행하는 음식 위주로 친절하게 음식을 추천해 주고 그 간단하게 음식 설명과 추천 이유를 설명해줍니다
-                 3개의 음식을 추천하고 먼저 음식이름들을 ,로 구분하여 []안에 넣어 말해줍니다 """
+System_Role="""당신은 한국에서 유행하는 음식 위주로 음식을 추천해 주고 간단하게 음식 설명과 추천 이유를 설명해줍니다
+                 3개의 음식을 추천하고 먼저 음식이름들을 []안에  ,로 구분하여 말해줍니다 ex) [삼계탕,불고기,스파게티]"""
     
 # 인자 user --> user에 해당하는 
 def food_by_gpt_api(user):
     weather = "맑음" # Weather.objects.get(user = user).status
     Prompt =f"저는 한국에 살고 오늘은 {date.today()}이고, 오늘의 날씨는 {weather}입니다. 오늘 음식 메뉴를 추천해주세요"
     openai.api_key = OPENAI_API_KEY
-
-    response = openai.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-        {"role": "system", "content": System_Role},
-        {"role": "user", "content": Prompt},
-        ],
-        max_tokens = 50,
-        temperature = 0.5
-    )   
-    return response.choices[0].message.content
-
+    print('???')
+    try:
+        response = openai.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+            {"role": "system", "content": System_Role},
+            {"role": "user", "content": Prompt},
+            ],
+            max_tokens = 500,
+            temperature = 0.5
+        )   
+        return response.choices[0].message.content
+    except Exception:
+        print('dk   ')
+        return ''
+    
 def food_names_from(text):
     start = text.find('[') + 1  
     end = text.find(']', start)  
--- apps/soccers/backend.py
+++ apps/soccers/backend.py
@@ -1,22 +1,24 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-# 웹 페이지에서 데이터를 가져옵니다.
-date = '2024-02-23'
-urls = []
-urls.append(f"https://sports.daum.net/schedule/epl")
-urls.append(f"https://sports.daum.net/schedule/primera")
-for url in urls:
-    page = requests.get(url)
+# 웹 페이지에서 데이터를 가져오기.
+date = '20240225' 
+url = (f"https://sports.daum.net/prx/hermes/api/game/schedule.json?page=1&leagueCode=epl%2Cprimera%2Cbundesliga%2Cseriea%2Cligue1%2Ceredivisie%2Cuefacl%2Cuefacup%2Cepl_cup%2Cfacup%2Ccopadelrey&fromDate={date}&toDate={date}")
+data = requests.get(url)
 
-    # BeautifulSoup 객체를 생성하여 HTML을 파싱합니다.
-    soup = bs(page.content, "html.parser")
-    print(soup)
-    # 'num_date' 클래스를 가진 모든 요소를 선택합니다.
-    # 이 부분은 실제 웹사이트의 구조에 따라 수정이 필요할 수 있습니다.
-    team_elements = soup.select('.tit_team')
-    # 선택된 각 요소의 텍스트를 출력합니다.
-    for team in team_elements:
-        print(team.text)
+# 가져온 json데이터를 파이썬 json객체로
+data = data.json()
 
+# print(data) # 데이터 출력시 딕셔너리형태
+#  딕셔너리 출력
+# for key,val in data.items():
+#     print(key)
+#     print(val)
 
+soccerSchedule = data['schedule'][date]
+
+for game in soccerSchedule:
+    #print(game)
+    print(game['startTime'])
+    print(game['homeTeamName'])
+    print(game['awayTeamName'])
